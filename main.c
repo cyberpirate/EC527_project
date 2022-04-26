428f2e2c5aaf2691cb5855c1b168a25b503e6171
Johnson <atjohnso@vlsi29.bu.edu>
2022-04-25 20:02:21 -0400

    debugging pthreads parallelization

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "oct_tree.h"
 #include "rand_gen.h"
 #include "utils.h"
+#include <pthread.h> // Multithreading
 
 #define PRINT_CALCULATING
 
@@ -22,11 +23,37 @@ void set_leaf_pos(struct OctTree* tree, struct Leaf* leaf) {
     mult_scalar(&leaf->velocity, TORUS_VEL/UNIVERSE_SIZE);
 }
 
+// Global tree structure for multithreading
+struct OctTree* tree;
+
+/*------------------------ Pthread functions ------------------------*/
+
+void *calc_force_threaded(void *ptr)
+{
+    calc_force(tree);
+    pthread_exit(NULL);
+}
+
+void *apply_force_threaded(void *ptr)
+{
+    apply_force(tree);
+    pthread_exit(NULL);
+}
+
+void *apply_velocity_threaded(void *ptr)
+{
+    apply_velocity(tree);
+    pthread_exit(NULL);
+}
+
+/*--------------------------------------------------------------------*/
+
 int main(int argc, char *argv[])
 {
     reset_rand();
 
-    struct OctTree* tree = create_tree(POINT_COUNT);
+    //struct OctTree* tree = create_tree(POINT_COUNT);
+    tree = create_tree(POINT_COUNT);
     double times[ITERS];
     struct timespec time_start, time_stop;
     struct timespec tot_time_start, tot_time_stop;
@@ -56,18 +83,26 @@ int main(int argc, char *argv[])
 #ifdef PRINT_CALCULATING
     printf("calculating: 0%%");
 #endif
+    pthread_t thread_id1[ITERS], thread_id2[ITERS], thread_id3[ITERS];
+
     for(int i = 0; i < ITERS; i++) {
         clock_gettime(CLOCK_REALTIME, &time_start);
         calc_center_of_mass(tree);
-        calc_force(tree);
-        apply_force(tree);
-        apply_velocity(tree);
+        //calc_force(tree);
+        pthread_create(&thread_id1[i], NULL, calc_force_threaded, NULL);
+        //apply_force(tree);
+        pthread_create(&thread_id2[i], NULL, apply_force_threaded, NULL);
+        //apply_velocity(tree);
+        pthread_create(&thread_id3[i], NULL, apply_velocity_threaded, NULL);
+        //pthread_join(thread_id1[i], NULL);
+        //pthread_join(thread_id2[i], NULL);
+        //pthread_join(thread_id3[i], NULL);
         rebalance(tree);
         clock_gettime(CLOCK_REALTIME, &time_stop);
         times[i] = interval(time_start, time_stop);
 
         fprintf(outputFile, "%d", i);
-        walk_leaves(tree, print_leaf_pos);
+        //walk_leaves(tree, print_leaf_pos);
         fprintf(outputFile, "\n");
 
 #ifdef PRINT_CALCULATING
@@ -77,6 +112,24 @@ int main(int argc, char *argv[])
         }
 #endif
     }
+
+    for (int j = 0; j < ITERS; j++)
+    {
+        pthread_join(thread_id1[j], NULL);
+        pthread_join(thread_id2[j], NULL);
+        pthread_join(thread_id3[j], NULL);
+    }
+/*
+    for (int j = 0; j < ITERS; j++)
+    {
+        pthread_join(thread_id2[j], NULL);
+    }
+
+    for (int j = 0; j < ITERS; j++)
+    {
+        pthread_join(thread_id3[j], NULL);
+    }
+*/
 #ifdef PRINT_CALCULATING
     printf("\rDone!\n");
 #endif
